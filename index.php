271266afed7c3f3f848c1f6600d4a06f47d95d32
ppodlas <45257627+ppodlas@users.noreply.github.com>
2018-12-06 22:26:01 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+include_once "connDB.php";
+$login = $_COOKIE['imie'];
+$plik = $_POST['plik'];
+$folder = $_POST['folder'];
+
+if ($folder == $login) {
+    $plik = $_POST['plik'];
+    if (isset($_POST['wyslij'])) {
+        $max_rozmiar = 5000;
+        if (is_uploaded_file($_FILES['plik']['tmp_name'])) {
+            if ($_FILES['plik']['size'] > $max_rozmiar) {
+                echo "Przekroczenie rozmiaru $max_rozmiar";
+            } else {
+                echo 'Odebrano plik: ' . $_FILES['plik']['name'] . '<br/>';
+                if (isset($_FILES['plik']['type'])) {
+                    echo 'Typ: ' . $_FILES['plik']['type'] . '<br/>';
+                }
+                move_uploaded_file($_FILES['plik']['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . "/zad7/Users/$login/" . $_FILES['plik']['name']);
+            }
+        } else {
+            echo '<span class="error" ">Błąd przy przesyłaniu danych!</span>';
+        }
+    }
+} else {
+    $plik = $_POST['plik'];
+    if (isset($_POST['wyslij'])) {
+        $max_rozmiar = 50000;
+        if (is_uploaded_file($_FILES['plik']['tmp_name'])) {
+            if ($_FILES['plik']['size'] > $max_rozmiar) {
+                echo "Przekroczenie rozmiaru $max_rozmiar";
+            } else {
+                echo '<span class="success">Odebrano plik:' . $_FILES['plik']['name'] . '<br/>';
+                if (isset($_FILES['plik']['type'])) {
+                    echo 'Typ: ' . $_FILES['plik']['type'] . '</span><br/>';
+                }
+                move_uploaded_file($_FILES['plik']['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . "/zad7/Users/$login/$folder/" . $_FILES['plik']['name']);
+            }
+        } else {
+            echo '<span class="error">Błąd przy przesyłaniu danych!</span>';
+        }
+    }
+}
+?>
 <html>
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
@@ -27,19 +71,19 @@
                 <div class="col">
                     <h3>Prześlij plik</h3>
                     <form method="post" ENCTYPE="multipart/form-data">
-                        <input type="file" name="plik"/>
-                        <input class="btn" type="submit" value="Wyślij plik"/>
+
                         <h5>Wybierz folder</h5>
-                        <select class="custom-select">
+                        <select name="folder" class="custom-select">
                             <?php
-                            $login = $_COOKIE['imie'];
+
                             foreach (glob("Users/$login/*", GLOB_ONLYDIR) as $dir) {
                                 $dir = str_replace("Users/$login/", '', $dir);
-                                echo '<option  name="km" value ="' . $dir . '"> ' . $dir . '<br>';
+                                echo '<option value ="' . $dir . '"> ' . $dir . '<br>';
                             }
                             ?>
                         </select>
-
+                        <input type="file" name="plik"/>
+                        <input class="btn" type="submit" name="wyslij" value="Wyślij plik"/>
                     </form>
                 </div>
             </td>
